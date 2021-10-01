f82f7b56634cccd0f8a9c5bc67357ad06519eff4
twinklesu <twinklesu914@gmail.com>
2021-10-01 17:25:42 +0900

    initial commit

---
+++ src/test/java/seoultech/gdsc/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package seoultech.gdsc.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
